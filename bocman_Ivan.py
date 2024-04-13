abadfe750f4bbf819d80d7597dee4aca5447fc3b
Ivan <165039394+bocman777iv@users.noreply.github.com>
2024-04-13 12:45:11 +0300

    Task8,9,10

--- bocman_Ivan.py
+++ bocman_Ivan.py
@@ -17,3 +17,8 @@ def discount_price(price, discount):
    
     return price
 # Task10
+def get_fullname (first_name, last_name, middle_name =""):
+    if middle_name:
+        return f"{first_name} {middle_name} {last_name}"
+    else:
+        return f"{first_name} {last_name}"
